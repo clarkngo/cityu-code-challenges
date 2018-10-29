3a4bfe1f2f81f6d4b9f42ed95c1e6d1eb2d4c3b5
Kevin W <rocwang0316@gmail.com>
2018-10-28 21:02:59 -0700

    Create Kevin-brute-force.java

---
+++ The Millionth Fibonacci Kata/Kevin-brute-force.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.util.*;
+
+// Brute force
+public class Fibonacci {
+  private final static BigInteger zero = new BigInteger("0");
+  private final static BigInteger one = new BigInteger("1");
+  private final static BigInteger two = new BigInteger("2");
+
+  public static BigInteger fib(BigInteger n) {
+    //Base case
+    if (n.compareTo(Fibonacci.zero) == 0) return Fibonacci.zero;
+    if (n.compareTo(Fibonacci.one) == 0) return Fibonacci.one;
+    
+    if (n.compareTo(Fibonacci.zero) == -1) return Fibonacci.fib(n.add(Fibonacci.two)).subtract(Fibonacci.fib(n.add(Fibonacci.one)));
+    return Fibonacci.fib(n.subtract(Fibonacci.one)).add(Fibonacci.fib(n.subtract(Fibonacci.two)));
+  }
+}
