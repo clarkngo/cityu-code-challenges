09baca09ff2234e5286856947a3d781d1ee262d6
clarkngo <clarkngo@gmail.com>
2018-12-16 18:20:51 +0800

    spinwords solutions

--- stop-gninnips-my-sdrow/clark-stop-gninnips-my-sdrow.js
+++ stop-gninnips-my-sdrow/clark-stop-gninnips-my-sdrow.js
@@ -17,4 +17,16 @@ function spinWords(str) {
   return arr.join(" ");
 }
 
+// best practice
+function spinWords(words){
+  return words.split(' ').map(function (word) {
+    return (word.length > 4) ? word.split('').reverse().join('') : word;
+  }).join(' ');
+}
+
+// regex
+function spinWords(string){
+  return string.replace(/\w{5,}/g, function(w) { return w.split('').reverse().join('') })
+}
+
 spinWords("Hello and goodbye");
