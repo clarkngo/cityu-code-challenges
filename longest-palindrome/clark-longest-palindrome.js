c1a6c37ff9130a10b1289b3ca6609d01e68d1ef7
clarkngo <clarkngo@gmail.com>
2018-12-20 10:15:22 +0800

    added other solutions

--- longest-palindrome/clark-longest-palindrome.js
+++ longest-palindrome/clark-longest-palindrome.js
@@ -74,3 +74,82 @@ longestPalindrome("aab");
 longestPalindrome("abcde");
 longestPalindrome("zzbaabcd");
 longestPalindrome("");
+
+// best practice
+longestPalindrome=function(s){
+  var longest = 0;
+  var length = s.length;
+
+  for(var i=0; i < length; i++){
+    for(var j = i+1; j <= length; j++) {
+      var str = s.slice(i,j);
+      var l = str.length
+      if(isPalindrome(str) && longest < l) {
+        longest = l;
+      }
+    }
+  }
+  return longest;
+}
+
+function isPalindrome(s) {
+  var arr = s.split("");
+  return s == arr.reverse().join("");
+}
+
+// other solution
+var longestPalindrome=function(s){
+  if (!s) return 0;
+  for (let c = s.length; c > 0; c--) {
+    for (let i = 0; i <= s.length - c; i++) {
+      var check = s.substr(i, c);
+      if (check === check.split("").reverse().join("")) return c;
+    }
+  }
+}
+
+// other solution
+longestPalindrome=function(s){
+  var l = 0;
+  for (var i = 0; i<s.length; i++)
+    for(var j = i; j<=s.length; j++)
+      if(s.substring(i,j) === s.substring(i,j).split('').reverse().join('') && s.substring(i,j).length > l)
+        l = s.substring(i,j).length;
+
+  return l;
+}
+
+// other solution
+longestPalindrome=function(s){
+  var a = s.split('');
+  var length = 0;
+  a.forEach(function(c, i){
+    length = Math.max(exploreArrayForImpairPalindrome(i, a), length);
+    length = Math.max(exploreArrayForPairPalindrome(i, a), length);
+  });
+  return length;
+}
+
+exploreArrayForImpairPalindrome=function(i, a){
+  var length = 1;
+  var iter = 1;
+  while(thoseValuesAreEquals(i,i,a,iter)){
+    length+=2;
+    iter++;
+  }
+  return length;
+}
+
+exploreArrayForPairPalindrome=function(i, a){
+  var length = 0;
+  var iter = 0;
+  while(thoseValuesAreEquals(i,i+1,a,iter)){
+    length+=2;
+    iter++;
+  }
+  return length;
+}
+
+thoseValuesAreEquals=function(i1, i2, a, iter){
+  return a[i1-iter] && a[i1-iter]===a[i2+iter];
+}
