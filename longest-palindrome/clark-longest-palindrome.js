38613ec16dd76dc8eba7de7517a483d3654f3677
clarkngo <clarkngo@gmail.com>
2018-12-20 10:13:21 +0800

    brute force longest palindrome

--- longest-palindrome/clark-longest-palindrome.js
+++ longest-palindrome/clark-longest-palindrome.js
@@ -1,6 +1,76 @@
 function longestPalindrome(str) {
-
-  
+  return checkPalindrome(str);
 }
 
+function checkPalindrome(str) {
+  if (str.length === 0) {
+    return 0;
+  }   
+  if (str.length === 2 && str[0] === str[1]) {
+    console.log(2);
+    return 2;
+  }
+  let longest = 1;    
+  let addminus = 1;
+  let temp = 0;
+
+  //  1 3 5
+  for (let i = 1; i < str.length - 1; i++) {
+    let j = i;
+    let k = i;
+
+    j -= addminus;
+    k += addminus;
+    while (j >= 0 && k < str.length && str[j] === str[k]) {
+     str.substring(j, k + 1);
+     j -= addminus;
+     k += addminus;
+     temp = k - j - 1;
+    }
+    if (temp > longest) {
+      longest = temp;
+    }
+  }
+  // 2 4 6
+  for (let i = 1; i < str.length - 1; i++) {
+    let j = i;
+    let k = i;
+
+    j -= addminus;
+    while (j >= 0 && k < str.length && str[j] === str[k]) {
+     str.substring(j, k + 1);
+     j -= addminus;
+     k += addminus;
+     temp = k - j - 1;
+    }
+    if (temp > longest) {
+      longest = temp;
+    }    
+  }
+  // 2 4 6
+  for (let i = 1; i < str.length - 1; i++) {
+    let j = i;
+    let k = i;
+
+    k += addminus;
+    while (j >= 0 && k < str.length && str[j] === str[k]) {
+     str.substring(j, k + 1);
+     j -= addminus;
+     k += addminus;
+     temp = k - j - 1;
+    }
+    if (temp > longest) {
+      longest = temp;
+    }    
+  }
+  return longest;
+}
+
+
+
+longestPalindrome("I like racecars that go fast");
+longestPalindrome("a");
+longestPalindrome("aab");
+longestPalindrome("abcde");
+longestPalindrome("zzbaabcd");
 longestPalindrome("");
